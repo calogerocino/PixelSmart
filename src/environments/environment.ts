23d883bca2362d686a3e131b3e2c24e2bda133ad
Double3C <kalogero.cino97@gmail.com>
2023-07-03 16:16:40 +0200

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'pixelsmart-6b352',
+    appId: '1:511052173876:web:bf3887067a6cb9cb32f061',
+    databaseURL: 'https://pixelsmart-6b352-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'pixelsmart-6b352.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDb836132HQAXB6suViTPCIPiGAOt3CzZ4',
+    authDomain: 'pixelsmart-6b352.firebaseapp.com',
+    messagingSenderId: '511052173876',
+    measurementId: 'G-9291PL1243',
+  },
+};
