586612b0467373a7706c8bbe59113c03331485b2
Double3C <kalogero.cino97@gmail.com>
2023-07-17 15:47:32 +0200

    source code cleanup

--- src/app/views/auth/login/login.component.ts
+++ src/app/views/auth/login/login.component.ts
@@ -1,43 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-// import { AuthService } from 'src/app/shared/guard/auth.service';
-import { AuthService } from "../../../shared/servizi/auth.service";
-
-import { NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
+import { AuthService } from '../../../shared/servizi/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
-  returnUrl: string;
-
-  constructor(private router: Router, private route: ActivatedRoute, private http: HttpClient, public authService:AuthService) { }
-
-  ngOnInit(): void {
-    // this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-  }
-
-
-  // onLoggedin(form: NgForm) {
-  //   const email = form.value.email;
-  //   const password = form.value.password;
-  //   this.authService.signIn(email, password).subscribe((data: any) => {
-  //     console.log(data);
-  //     const expirationDate = new Date(
-  //       new Date().getTime() + data.expiresIn * 1000
-  //     );
-  //     this.authService.createUser(
-  //       data.email,
-  //       data.localId,
-  //       data.idToken,
-  //       expirationDate
-  //     );
-  //     this.authService.isLoggedIn = true;
-  //     localStorage.setItem('user', JSON.stringify(this.authService.user));
-  //     this.router.navigate([this.returnUrl]);
-  //   });
-  // }
+export class LoginComponent {
+  constructor(public authService: AuthService) {}
 }
--- src/app/views/auth/register/register.component.ts
+++ src/app/views/auth/register/register.component.ts
@@ -1,35 +1,11 @@
-import { Component, OnInit } from "@angular/core";
-// import { Router } from "@angular/router";
-// import { AuthModule } from "../auth.module";
+import { Component } from "@angular/core";
 import { AuthService } from "../../../shared/servizi/auth.service";
-// import { NgForm } from "@angular/forms";
-import Swal from "sweetalert2";
 
 @Component({
   selector: "app-register",
   templateUrl: "./register.component.html",
   styleUrls: ["./register.component.scss"],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
   constructor(public authService: AuthService) {}
-
-  ngOnInit(): void {}
-
-  // onRegister(form: NgForm) {
-  //   const email = form.value.email;
-  //   const password = form.value.password;
-  //   this.authService
-  //     .signUp(email, password)
-  //     .subscribe((data) => console.log(data));
-
-  //   const Toast = Swal.mixin({
-  //     toast: true,
-  //     position: 'top-end',
-  //     timer: 3000,
-  //     timerProgressBar: true
-  //   })
-  //   Toast.fire(`Registrato con successo, attendi l'approvazione per potere accedere!`, '', 'info')
-
-  //   this.router.navigate(["/auth/login"]);
-  // }
 }
